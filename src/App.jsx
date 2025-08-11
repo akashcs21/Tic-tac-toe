31cea9ed2451982a98bb9dfaa844e825aa1a63c3
Akash Rathore <akashrathore8118@gmail.com>
2025-08-11 15:32:47 +0530

    app.js

--- src/App.jsx
+++ src/App.jsx
@@ -77,7 +77,7 @@ function App() {
           <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} onNameChange={handlePlayerNamechange} />
 
         </ol>
-      console.log("Or btao git add hui kya!")
+     
         {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart} />}
 
         <GameBoard onSelect={handleSelectSquare} board={gameBoard} />
